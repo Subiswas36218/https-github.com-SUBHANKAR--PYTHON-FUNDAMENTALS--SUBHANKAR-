81238fb487770b3029ed39ab5397665ec6bd4b66
SUBHANKAR BISWAS <subhankarbiswas@SUBHANKARs-MacBook-Air.local>
2025-11-15 03:59:22 +0530

    Mongoengine models for articles

--- src/models/mongo.py
+++ src/models/mongo.py
@@ -1,13 +1,13 @@
 from mongoengine import EmbeddedDocument, IntField, StringField, DateTimeField, EmbeddedDocumentField, Document
 
 
-class Author(EmbeddedDocument):
+class Author(EmbeddedDocument): # type: ignore[misc]
     id = IntField(required=True)
     full_name = StringField()
     title = StringField()
     
 
-class ScientificArticle(Document):
+class ScientificArticle(Document): # type: ignore[misc]
     meta = {"collection": "articles", "indexes": ["id", "arxiv_id"]}
     id = IntField(required=True)
     
