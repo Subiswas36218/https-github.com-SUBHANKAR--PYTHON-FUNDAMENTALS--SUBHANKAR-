5e9f9f386fef4d7055985385e3ce4c7c153aa339
SUBHANKAR BISWAS <subhankarbiswas@SUBHANKARs-MacBook-Air.local>
2025-11-15 18:07:28 +0530

    export of the articles to mongo

--- src/usecases/load_from_mongo.py
+++ src/usecases/load_from_mongo.py
@@ -1,10 +1,10 @@
+from typing import List
+
 from src.models.mongo import ScientificArticle
 import src.storage.mongo  # noqa: F401
 
-
-def list_articles():
-    return ScientificArticle.objects.all()
-
+def list_articles() -> List[ScientificArticle]:
+    return ScientificArticle.objects.all()  # type: ignore[return-value]
 
 if __name__ == "__main__":
     print(list_articles())
